397155b6dd96a846ca3308d997b51590083bdaff
Przemyslaw Kot <przemekkot@wp.pl>
2020-05-21 20:57:59 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+num = 30
+fibonacci = [1, 1]
+counter = 1
+
+for number in fibonacci:
+  number = fibonacci[-1] + fibonacci[-2]
+  counter += 1
+  if counter == 30:
+    break
+  fibonacci.append(number)
+  
+print(fibonacci)
